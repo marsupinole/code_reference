4d2aa1af48f9469cc0014177c186d13af7168dd5
Michael Leveton <mleveton@gmail.com>
2012-03-15 01:59:54 -0400

    final total

--- ruby/temp3.rb
+++ ruby/temp3.rb
@@ -1,83 +1,28 @@
-#make two arrays, for each R, find the S's in that time range, add the scores, use permutation 
-#script you found and find the maximum score that fits the browser height
+array = [[[1.6666666666666667, 1, 30, 50, 11]], [[1.125, 3, 40, 45, 14], [1.6666666666666667, 1, 30, 50, 11], [2.0, 2, 20, 40, 13]], [[1.125, 3, 40, 45, 14], [1.6666666666666667, 1, 30, 50, 11], [2.0, 2, 20, 40, 13]], [[1.6666666666666667, 1, 30, 50, 11], [2.0, 2, 20, 40, 13], [2.25, 4, 20, 45, 18]], [[1.125, 3, 40, 45, 14], [2.0, 2, 20, 40, 13], [2.25, 4, 20, 45, 18]]]
 
-#9 10 100
-#S 11 50 30
-#R 12
-#S 13 40 20
-#S 14 45 40
-#R 15
-#R 16
-#S 18 45 20
-#R 21
-#R 22
-
-def arrays_to_i(array)
-  y = 0
-  while y < array.length
-    array[y].map!(&:to_i)
-    y += 1
-  end
-  array
+y = 0 
+while y < array.length
+  array[y].map(&:shift)
+  array[y].map(&:pop)
+  array[y].map{|x| x.slice!(1)}
+  y += 1
 end
 
-def quora_feed_optimizer(meta_array, story_array, reload_array)
-  minimum_times = []
-  aggregate_array = []
-    reload_array.flatten! #=> [12, 15, 16, 21, 22]
-    $reload_quantity = reload_array.length
-    
-    i = 0
-    while i < reload_array.length
-      minimum_times.push(reload_array[i] - 10)
-      i += 1
-    end #=> [2, 5, 6, 11, 12]
-    
-    story_array.map{|x| x.push(story_array.index(x) + 1)} #=> [[11, 50, 30, 1], [13, 40, 20, 2], [14, 45, 40, 3], [18, 45, 20, 4]]
-    story_array.map!{|x| x.push(x[1].to_f / x[2].to_f)} #=> [[11, 50, 30, 1, 1.6666666666666667], [13, 40, 20, 2, 2.0], [14, 45, 40, 3, 1.125], [18, 45, 20, 4, 2.25]]
-    
-    #x = 0
-    #while x < minimum_times.length
-      #aggregate_array.push(story_array.take_while{|m| m[0] >= minimum_times[x] and m[0] <= (minimum_times[x] + 10)})
-      #x += 1
-    #end
+#print array
 
-    
+#array.map!{|x| x.push(x.length)}
 
-    print reload_array #=> [[[11, 50, 30, 1, 1.6666666666666667]], [[11, 50, 30, 1, 1.6666666666666667], [13, 40, 20, 2, 2.0], [14, 45, 40, 3, 1.125]], [[11, 50, 30, 1, 1.6666666666666667], [13, 40, 20, 2, 2.0], [14, 45, 40, 3, 1.125]], [], []]
-
-    #story_arrayInTime = story_array.take_while {|x| x[0] > reload_min_time and x[0] < reload_array[0]} #=> [[11, 50, 30]]
-  #print meta_array
-  #shift out the time for this set, index the array, divide scores by browser height - the bigger the better
-  # sort and reverse based on this new meta score until browser limit is reached 
+#print array
+def final_total(array)
+	#hash array
+	#final_array.push(values total)
+	#final_array.push(keys.length)
+	#final_array.push(keys)
 end
 
+array.map!{|x| final_total(x)}
+
+#array.map!(&:sort!)
 
 
-stdin_input = ["9 10 100", "S 11 50 30", "R 12", "S 13 40 20", "S 14 45 40", "R 15", "R 16", "S 18 45 20", "R 21", "R 22"]
-
-meta_array = stdin_input[0].split(' ').map!(&:to_i)
-
-stdin_input.shift
-
-stdin_input.map! {|x| x.split(' ')}
-
-story_array = []
-reload_array = []
-i = 0
-while i < stdin_input.length
-  if stdin_input[i][0] == "S"
-    story_array.push(stdin_input[i])
-  else
-    reload_array.push(stdin_input[i])
-  end
-  i += 1
-end
-   
-story_array.each(&:shift) #=> [["11", "50", "30"], ["13", "40", "20"], ["14", "45", "40"], ["18", "45", "20"]]
-
-reload_array.each(&:shift) #=> [["12"], ["15"], ["16"], ["21"], ["22"]]
-
-arrays_to_i(story_array) and arrays_to_i(reload_array)
-
-quora_feed_optimizer(meta_array, story_array, reload_array)
+#a.map{|x| x.slice!(1)}
